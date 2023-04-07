ddb7d1018de012c08f728b483d144b19e28eb5c3
Vince <vincivenv@gmail.com>
2023-04-08 01:04:33 +0200

    Time: 0 ms (100.00%) | Memory: 2.2 MB (47.73%) - LeetSync

--- valid-parentheses/valid-parentheses.rs
+++ valid-parentheses/valid-parentheses.rs
@@ -1,10 +1,17 @@
+// Time complexity
+// O(n), where n is the length of the input string s
+// Space complexity
+// O(n), e.g in the worst case (when s consists entirely of opening brackets)
+
 impl Solution {
     pub fn is_valid(s: String) -> bool {
         let brackets: std::collections::HashMap<char, char> = [(')', '('), ('}', '{'), (']', '[')]
             .iter()
             .cloned()
             .collect();
-
+        if s.len() % 2 != 0 {
+            return false;
+        }
         let mut stack = Vec::new();
         for bracket in s.chars() {
             match bracket {
