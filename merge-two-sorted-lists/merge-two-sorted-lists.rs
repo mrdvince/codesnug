ba9aeabedfeca69d8e1103be9fb3e61b76e24af6
Vince <vincivenv@gmail.com>
2023-04-08 02:00:39 +0200

    Time: 0 ms (100.00%) | Memory: 2.3 MB (9.00%) - LeetSync

--- merge-two-sorted-lists/merge-two-sorted-lists.rs
+++ merge-two-sorted-lists/merge-two-sorted-lists.rs
@@ -16,26 +16,24 @@
 // }
 impl Solution {
     pub fn merge_two_lists(
-        list1: Option<Box<ListNode>>,
-        list2: Option<Box<ListNode>>,
+        mut list1: Option<Box<ListNode>>,
+        mut list2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
         let mut dummy = ListNode::new(0);
         let mut tail = &mut dummy;
-        let mut l1 = list1;
-        let mut l2 = list2;
 
-        while let (Some(node1), Some(node2)) = (l1.as_ref(), l2.as_ref()) {
+        while let (Some(node1), Some(node2)) = (list1.as_ref(), list2.as_ref()) {
             if node1.val <= node2.val {
                 tail.next = Some(Box::new(ListNode::new(node1.val)));
-                l1 = node1.next.clone();
+                list1 = node1.next.clone();
             } else {
                 tail.next = Some(Box::new(ListNode::new(node2.val)));
-                l2 = node2.next.clone();
+                list2 = node2.next.clone();
             }
             tail = tail.next.as_mut().unwrap();
         }
 
-        tail.next = if l1.is_some() { l1 } else { l2 };
+        tail.next = if list1.is_some() { list1 } else { list2 };
         dummy.next
     }
 }
