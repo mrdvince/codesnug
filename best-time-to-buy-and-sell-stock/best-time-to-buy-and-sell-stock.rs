55aaaf3dd85b149e188f8e15a449aead8add0d58
Vince <vincivenv@gmail.com>
2023-04-10 18:41:03 +0200

    Time: 17 ms (31.40%) | Memory: 2.9 MB (66.28%) - LeetSync

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.rs
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.rs
@@ -2,10 +2,9 @@ impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let (mut curr_profit, mut max_profit) = (0, 0);
         for index in 1..prices.len() {
-            curr_profit = curr_profit + prices[index] - prices[index - 1];
             // if negative reset to zero
-            curr_profit = std::cmp::max(0, curr_profit);
-            max_profit = std::cmp::max(curr_profit, max_profit)
+            curr_profit = curr_profit.max(0) + prices[index] - prices[index - 1];
+            max_profit = max_profit.max(curr_profit);
         }
         max_profit
     }
