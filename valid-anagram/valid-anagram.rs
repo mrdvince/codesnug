9ec36413f4febedbe2f65c9731215aeb14158b42
Vince <vincivenv@gmail.com>
2023-04-13 05:32:17 +0200

    Time: 4 ms (49.81%) | Memory: 2.2 MB (61.71%) - LeetSync

--- valid-anagram/valid-anagram.rs
+++ valid-anagram/valid-anagram.rs
@@ -1,18 +1,20 @@
 use std::collections::HashMap;
 impl Solution {
     pub fn is_anagram(s: String, t: String) -> bool {
-        if s.len() != t.len() {
-            return false;
+        let mut counts = HashMap::new();
+        for ch in s.chars() {
+            *counts.entry(ch).or_insert(0) += 1;
         }
-        let s_counts = s.chars().fold(HashMap::new(), |mut counts, ch| {
-            *counts.entry(ch).or_insert(0) += 1;
-            counts
-        });
-
-        let t_counts = t.chars().fold(HashMap::new(), |mut counts, ch| {
-            *counts.entry(ch).or_insert(0) += 1;
-            counts
-        });
-        s_counts == t_counts
+        for ch in t.chars() {
+            if let Some(count) = counts.get_mut(&ch) {
+                *count -= 1;
+                if *count < 0 {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        counts.values().all(|&count| count == 0)
     }
 }
