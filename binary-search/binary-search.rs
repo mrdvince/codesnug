8b3edb965d5b0c232a90bca61b40bb080c1affef
Vince <vincivenv@gmail.com>
2023-04-15 12:27:41 +0200

    Time: 0 ms (100.00%) | Memory: 2.2 MB (80.88%) - LeetSync

--- binary-search/binary-search.rs
+++ binary-search/binary-search.rs
@@ -1,17 +1,13 @@
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        let mut lo: i32 = 0;
-        let mut hi = (nums.len() - 1) as i32;
-
+        let mut lo = 0;
+        let mut hi = nums.len() as i32 - 1;
         while lo <= hi {
-            let mid = (lo + hi) / 2;
-            if target == nums[mid as usize] {
-                return mid;
-            }
-            if nums[mid as usize] < target {
-                lo = mid + 1;
-            } else {
-                hi = mid - 1
+            let mid = lo + (hi - lo) / 2;
+            match nums.get(mid as usize) {
+                Some(&mid_val) if mid_val == target => return mid as i32,
+                Some(&mid_val) if mid_val < target => lo = mid + 1,
+                _ => hi = mid - 1,
             }
         }
         -1
