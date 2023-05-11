bbee2e99faf528f546b350833458275653724b87
Vince <vincivenv@gmail.com>
2023-05-11 07:42:33 +0200

    Time: 1 ms (81.23%), Space: 2 MB (65.52%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.rs
@@ -0,0 +1,31 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        if head.is_none() || head.as_ref().unwrap().next.is_none() {
+            return head;
+        }
+        let mut slow = head.clone();
+        let mut fast = head.clone();
+
+        while fast.is_some() && fast.as_ref().unwrap().next.is_some() {
+            slow = slow.unwrap().next;
+            fast = fast.unwrap().next.unwrap().next;
+        }
+        slow
+    }
+}
