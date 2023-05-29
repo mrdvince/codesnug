53167f9f9fd5faacde1d62876095fdb63a28f773
Vince <vincivenv@gmail.com>
2023-05-29 11:00:47 +0200

    Time: 13 ms (57.32%), Space: 3.3 MB (18.29%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.rs
+++ 0053-maximum-subarray/0053-maximum-subarray.rs
@@ -1,20 +1,11 @@
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-        if nums.iter().max().unwrap() < &0 {
-            return *nums.iter().max().unwrap();
+        let mut max_so_far = nums[0];
+        let mut max_ending_here = nums[0];
+        for i in 1..nums.len() {
+            max_ending_here = nums[i].max(max_ending_here + nums[i]);
+            max_so_far = max_so_far.max(max_ending_here);
         }
-
-        let mut max_sum = nums[0];
-        let mut curr_sum = 0;
-
-        for num in nums {
-            if curr_sum < 0 {
-                curr_sum = 0;
-            }
-            curr_sum += num;
-            max_sum = std::cmp::max(max_sum, curr_sum);
-            println!("{}, {}", max_sum, curr_sum);
-        }
-        max_sum
+        max_so_far
     }
 }
