fadd32a486e53267fd0f7f05faff737e059dd8e9
Vince <vincivenv@gmail.com>
2023-05-29 11:03:07 +0200

    Time: 23 ms (5.49%), Space: 3.2 MB (47.87%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.rs
+++ 0053-maximum-subarray/0053-maximum-subarray.rs
@@ -1,11 +1,40 @@
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-        let mut max_so_far = nums[0];
-        let mut max_ending_here = nums[0];
-        for i in 1..nums.len() {
-            max_ending_here = nums[i].max(max_ending_here + nums[i]);
-            max_so_far = max_so_far.max(max_ending_here);
+        Self::helper(&nums, 0, nums.len() - 1)
+    }
+    
+    fn helper(nums: &[i32], left: usize, right: usize) -> i32 {
+        if left == right {
+            return nums[left];
         }
-        max_so_far
+        
+        let mid = (left + right) / 2;
+        let left_sum = Self::helper(nums, left, mid);
+        let right_sum = Self::helper(nums, mid + 1, right);
+        let cross_sum = Self::cross_sum(nums, left, right, mid);
+        
+        left_sum.max(right_sum).max(cross_sum)
+    }
+    
+    fn cross_sum(nums: &[i32], left: usize, right: usize, mid: usize) -> i32 {
+        if left == right {
+            return nums[left];
+        }
+        
+        let mut left_subsum = i32::MIN;
+        let mut curr_sum = 0;
+        for i in (left..=mid).rev() {
+            curr_sum += nums[i];
+            left_subsum = left_subsum.max(curr_sum);
+        }
+        
+        let mut right_subsum = i32::MIN;
+        curr_sum = 0;
+        for i in mid + 1..=right {
+            curr_sum += nums[i];
+            right_subsum = right_subsum.max(curr_sum);
+        }
+        
+        left_subsum + right_subsum
     }
 }
