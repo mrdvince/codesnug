e183c65e7fafb1b110e4ed92e2ee23567f451cc5
Vince <vincivenv@gmail.com>
2023-07-24 07:57:42 +0200

    Time: 0 ms (100.00%), Space: 2.3 MB (6.09%) - LeetHub

--- 0198-house-robber/0198-house-robber.rs
+++ 0198-house-robber/0198-house-robber.rs
@@ -1,5 +1,11 @@
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
+        if nums.len() == 0 {
+            return 0;
+        }
+        if nums.len() == 1 {
+            return nums[0];
+        }
         let (mut loot1, mut loot2) = (0, 0);
         
         for num in nums {
