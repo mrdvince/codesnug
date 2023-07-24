3223a81dca2b32b24c9b6aeeeb7a53b12714a5b7
Vince <vincivenv@gmail.com>
2023-07-24 06:16:59 +0200

    Time: 1 ms (82.74%), Space: 1.9 MB (91.88%) - LeetHub

--- 0198-house-robber/0198-house-robber.rs
+++ 0198-house-robber/0198-house-robber.rs
@@ -1,21 +1,12 @@
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
-        let n = nums.len();
-        if n == 0 {
-            return 0;
+        let (mut loot1, mut loot2) = (0, 0);
+        
+        for num in nums {
+            let temp = loot1;
+            loot1 = std::cmp::max(loot2 + num, loot1);
+            loot2 = temp;
         }
-        if n == 1 {
-            return nums[0];
-        }
-
-        let mut dp: Vec<i32> = vec![0; n];
-        dp[0] = nums[0];
-        dp[1] = std::cmp::max(nums[0], nums[1]);
-
-        for i in 2..n {
-            dp[i] = std::cmp::max(nums[i] + dp[i - 2], dp[i - 1]);
-        }
-
-        dp[n - 1]
+        loot1
     }
 }
