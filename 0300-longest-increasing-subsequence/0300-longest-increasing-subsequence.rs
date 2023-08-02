6d6712802c7dd1947d96043066910d83389671ac
Vince <vincivenv@gmail.com>
2023-08-02 10:38:55 +0200

    Time: 2 ms (89.66%), Space: 2.1 MB (79.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.rs
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.rs
@@ -1,27 +1,20 @@
 impl Solution {
     pub fn length_of_lis(nums: Vec<i32>) -> i32 {
-        let mut memo = vec![vec![-1; nums.len()]; nums.len() + 1];
-        Solution::lis(&nums, -1, 0, &mut memo)
-    }
-    
-    fn lis(nums: &Vec<i32>, prev_index: i32, curr_index: usize, memo: &mut Vec<Vec<i32>>) -> i32 {
-        if curr_index == nums.len() {
-            return 0;
-        }
-
-        if prev_index >= 0 && memo[(prev_index + 1) as usize][curr_index] >= 0 {
-            return memo[(prev_index + 1) as usize][curr_index];
-        }
-
-        let taken = if prev_index < 0 || nums[curr_index] > nums[prev_index as usize] {
-            1 + Solution::lis(nums, curr_index as i32, curr_index + 1, memo)
-        } else {
-            0
-        };
+        let mut tails = vec![];
         
-        let not_taken = Solution::lis(nums, prev_index, curr_index + 1, memo);
-
-        memo[(prev_index + 1) as usize][curr_index] = std::cmp::max(taken, not_taken);
-        memo[(prev_index + 1) as usize][curr_index]
+        for num in nums {
+            match tails.binary_search(&num) {
+                Ok(i) => tails[i] = num,
+                Err(i) => {
+                    if i == tails.len() {
+                        tails.push(num);
+                    } else {
+                        tails[i] = num;
+                    }
+                }
+            }
+        }
+        
+        tails.len() as i32
     }
 }
