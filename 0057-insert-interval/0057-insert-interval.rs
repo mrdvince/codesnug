cc14a1c8521e31bc30a7f63b4de455b5afc499ed
Vince <vincivenv@gmail.com>
2023-08-04 12:40:09 +0200

    Update 0057-insert-interval.rs

--- 0057-insert-interval/0057-insert-interval.rs
+++ 0057-insert-interval/0057-insert-interval.rs
@@ -1,29 +1,51 @@
 impl Solution {
     pub fn insert(intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
-        let (mut start, mut end) = (new_interval[0], new_interval[1]);
-        let mut i = 0;
         let mut result: Vec<Vec<i32>> = Vec::new();
+        let mut new_interval = new_interval;
 
-        // add all the intervals ending before start of new_interval to result
-        while i < intervals.len() && intervals[i][1] < start {
-            result.push(intervals[i].clone());
-            i += 1;
+        for interval in intervals.into_iter() {
+            if new_interval[1] < interval[0] {
+                result.push(new_interval.clone());
+                new_interval = interval;
+            } else if interval[1] < new_interval[0] {
+                result.push(interval);
+            } else {
+                new_interval[0] = new_interval[0].min(interval[0]);
+                new_interval[1] = new_interval[1].max(interval[1]);
+            }
         }
 
-        // merge all overlapping intervals
-        while i < intervals.len() && intervals[i][0] <= end {
-            start = std::cmp::min(start, intervals[i][0]);
-            end = std::cmp::max(end, intervals[i][1]);
-            i += 1;
-        }
-        result.push(vec![start, end]);
-
-        // add remaining intervals
-        while i < intervals.len() {
-            result.push(intervals[i].clone());
-            i += 1;
-        }
-
+        result.push(new_interval);
         result
     }
 }
+
+
+// impl Solution {
+//     pub fn insert(intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {
+//         let mut left = 0;
+//         let mut right = intervals.len();
+        
+//         while left < right {
+//             let mid = left + (right - left) / 2;
+//             if intervals[mid][0] < new_interval[0] {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid;
+//             }
+//         }
+        
+//         let mut intervals = intervals;
+//         intervals.insert(left, new_interval);
+        
+//         let mut result = vec![intervals[0].clone()];
+//         for interval in intervals.into_iter().skip(1) {
+//             if interval[0] > result.last().unwrap()[1] {
+//                 result.push(interval);
+//             } else {
+//                 result.last_mut().unwrap()[1] = result.last().unwrap()[1].max(interval[1]);
+//             }
+//         }
+//         result
+//     }
+// }
