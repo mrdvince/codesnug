5997ba4f4971379970dc60fbf2ae6c2daa27a82c
Vince <vincivenv@gmail.com>
2023-08-04 11:30:25 +0200

    Time: 538 ms (5.23%), Space: 2 MB (96.27%) - LeetHub

--- 0055-jump-game/0055-jump-game.rs
+++ 0055-jump-game/0055-jump-game.rs
@@ -1,15 +1,18 @@
 impl Solution {
     pub fn can_jump(nums: Vec<i32>) -> bool {
-        let mut max_reachable = 0;
-        for (i, &num) in nums.iter().enumerate() {
-            if i > max_reachable {
-                return false;
-            }
-            max_reachable = max_reachable.max(i + num as usize);
-            if max_reachable >= nums.len() - 1 {
-                return true;
+        // DP
+        let n = nums.len();
+        let mut dp = vec![false;n];
+        dp[0] = true;
+        
+        for i in 1..n {
+            for j in 0..i {
+                if dp[j] && j + nums[j] as usize >= i {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        false
+        dp[n-1]
     }
 }
