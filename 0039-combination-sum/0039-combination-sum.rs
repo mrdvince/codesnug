96550cc600e4da8ba0ac6f8ac54617d93f33d6a6
Vince <vincivenv@gmail.com>
2023-08-09 07:19:56 +0200

    Time: 0 ms (100.00%), Space: 2.2 MB (36.74%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.rs
+++ 0039-combination-sum/0039-combination-sum.rs
@@ -1,28 +1,59 @@
+// DFS
+// TC: O(N^T)
 impl Solution {
     pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
         let mut result = Vec::new();
-        let mut combination = Vec::new();
-        Solution::backtrack(&candidates, target, 0, &mut combination, &mut result);
+        Solution::dfs(&candidates, target, 0, &mut Vec::new(), &mut result);
         result
     }
-    fn backtrack(
+    fn dfs(
         candidates: &Vec<i32>,
         target: i32,
         start: usize,
-        combination: &mut Vec<i32>,
+        current: &mut Vec<i32>,
         result: &mut Vec<Vec<i32>>,
     ) {
         if target == 0 {
-            result.push(combination.clone());
-            return;
-        }
-        if target < 0 {
+            result.push(current.clone());
             return;
         }
         for i in start..candidates.len() {
-            combination.push(candidates[i]);
-            Solution::backtrack(candidates, target - candidates[i], i, combination, result);
-            combination.pop();
+            if candidates[i] <= target {
+                current.push(candidates[i]);
+                Solution::dfs(candidates, target - candidates[i], i, current, result);
+                current.pop();
+            }
         }
     }
 }
+
+// Bactrack
+// TC: O(N^T)
+// impl Solution {
+//     pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+//         let mut result = Vec::new();
+//         let mut combination = Vec::new();
+//         Solution::backtrack(&candidates, target, 0, &mut combination, &mut result);
+//         result
+//     }
+//     fn backtrack(
+//         candidates: &Vec<i32>,
+//         target: i32,
+//         start: usize,
+//         combination: &mut Vec<i32>,
+//         result: &mut Vec<Vec<i32>>,
+//     ) {
+//         if target == 0 {
+//             result.push(combination.clone());
+//             return;
+//         }
+//         if target < 0 {
+//             return;
+//         }
+//         for i in start..candidates.len() {
+//             combination.push(candidates[i]);
+//             Solution::backtrack(candidates, target - candidates[i], i, combination, result);
+//             combination.pop();
+//         }
+//     }
+// }
