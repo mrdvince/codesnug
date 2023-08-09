bb2350faf68a48fb173446452091933aef0a3a50
Vince <vincivenv@gmail.com>
2023-08-09 07:26:40 +0200

    Time: 0 ms (100.00%), Space: 2.1 MB (60.93%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.rs
+++ 0039-combination-sum/0039-combination-sum.rs
@@ -1,26 +1,26 @@
-// TC: O(c⋅t⋅2 ^t)
-impl Solution {
-    pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-        let target = target as usize;
-        let mut dp: Vec<Vec<Vec<i32>>> = vec![Vec::new(); target + 1];
-        dp[0].push(vec![]);
+// // TC: O(c⋅t⋅2 ^t)
+// impl Solution {
+//     pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+//         let target = target as usize;
+//         let mut dp: Vec<Vec<Vec<i32>>> = vec![Vec::new(); target + 1];
+//         dp[0].push(vec![]);
         
-        for &candidate in &candidates {
-            let candidate = candidate as usize;
-            for i in candidate..=target {
-                let mut new_combinations = Vec::new();
-                for previous_combination in &dp[i - candidate] {
-                    let mut new_combination = previous_combination.clone();
-                    new_combination.push(candidate as i32);
-                    new_combinations.push(new_combination);
-                }
-                dp[i].append(&mut new_combinations);
-            }
-        }
+//         for &candidate in &candidates {
+//             let candidate = candidate as usize;
+//             for i in candidate..=target {
+//                 let mut new_combinations = Vec::new();
+//                 for previous_combination in &dp[i - candidate] {
+//                     let mut new_combination = previous_combination.clone();
+//                     new_combination.push(candidate as i32);
+//                     new_combinations.push(new_combination);
+//                 }
+//                 dp[i].append(&mut new_combinations);
+//             }
+//         }
         
-        dp[target].clone()
-    }
-}
+//         dp[target].clone()
+//     }
+// }
 
 // DFS
 // TC: O(N^T)
@@ -53,31 +53,31 @@ impl Solution {
 
 // Bactrack
 // TC: O(N^T)
-// impl Solution {
-//     pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-//         let mut result = Vec::new();
-//         let mut combination = Vec::new();
-//         Solution::backtrack(&candidates, target, 0, &mut combination, &mut result);
-//         result
-//     }
-//     fn backtrack(
-//         candidates: &Vec<i32>,
-//         target: i32,
-//         start: usize,
-//         combination: &mut Vec<i32>,
-//         result: &mut Vec<Vec<i32>>,
-//     ) {
-//         if target == 0 {
-//             result.push(combination.clone());
-//             return;
-//         }
-//         if target < 0 {
-//             return;
-//         }
-//         for i in start..candidates.len() {
-//             combination.push(candidates[i]);
-//             Solution::backtrack(candidates, target - candidates[i], i, combination, result);
-//             combination.pop();
-//         }
-//     }
-// }
+impl Solution {
+    pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        let mut result = Vec::new();
+        let mut combination = Vec::new();
+        Solution::backtrack(&candidates, target, 0, &mut combination, &mut result);
+        result
+    }
+    fn backtrack(
+        candidates: &Vec<i32>,
+        target: i32,
+        start: usize,
+        combination: &mut Vec<i32>,
+        result: &mut Vec<Vec<i32>>,
+    ) {
+        if target == 0 {
+            result.push(combination.clone());
+            return;
+        }
+        if target < 0 {
+            return;
+        }
+        for i in start..candidates.len() {
+            combination.push(candidates[i]);
+            Solution::backtrack(candidates, target - candidates[i], i, combination, result);
+            combination.pop();
+        }
+    }
+}
