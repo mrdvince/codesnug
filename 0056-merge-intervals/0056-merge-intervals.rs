6c35800dd8cb6dc7cdabcfc79a08d6c54ca708aa
Vince <vincivenv@gmail.com>
2023-08-23 10:33:54 +0200

    Time: 3 ms (95.92%), Space: 3.2 MB (38.27%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.rs
+++ 0056-merge-intervals/0056-merge-intervals.rs
@@ -1,21 +1,20 @@
 impl Solution {
-    pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         if intervals.is_empty() {
             return Vec::new();
         }
         
-        let mut intervals = intervals.clone();
         intervals.sort_by(|a, b| a[0].cmp(&b[0]));
         
         let mut result: Vec<Vec<i32>> = Vec::new();
         result.push(intervals[0].clone());
         
-        for i in 1..intervals.len() {
+        for interval in &intervals[1..] {
             let last_interval = result.last_mut().unwrap();
-            if last_interval[1] < intervals[i][0] {
-                result.push(intervals[i].clone());
+            if last_interval[1] < interval[0] {
+                result.push(interval.clone());
             } else {
-                last_interval[1] = last_interval[1].max(intervals[i][1]);
+                last_interval[1] = last_interval[1].max(interval[1]);
             }
         }
         
