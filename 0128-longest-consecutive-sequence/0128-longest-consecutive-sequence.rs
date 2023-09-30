0e91608b3d7c292493b9d0255e3121e76a60ece7
Vince <vincivenv@gmail.com>
2023-09-30 10:37:53 +0200

    Time: 1608 ms (5.18%), Space: 3.4 MB (79.28%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.rs
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.rs
@@ -1,23 +1,22 @@
 impl Solution {
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
-        use std::collections::HashMap;
-        let mut map = HashMap::new();
-        let mut max_length = 0;
+        let num_set: std::collections::HashSet<i32> = nums.iter().cloned().collect();
+        let mut max_streak = 0;
 
         for &num in &nums {
-            if !map.contains_key(&num) {
-                let left = *map.get(&(num - 1)).unwrap_or(&0);
-                let right = *map.get(&(num + 1)).unwrap_or(&0);
+            if !num_set.contains(&(num - 1)) {
+                let mut current_num = num;
+                let mut current_streak = 1;
 
-                let current_length = left + right + 1;
-                map.insert(num, current_length);
+                while num_set.contains(&(current_num + 1)) {
+                    current_num += 1;
+                    current_streak += 1;
+                }
 
-                max_length = std::cmp::max(max_length, current_length);
-                map.insert(num - left, current_length);
-                map.insert(num + right, current_length);
+                max_streak = std::cmp::max(max_streak, current_streak);
             }
         }
 
-        max_length
+        max_streak
     }
 }
