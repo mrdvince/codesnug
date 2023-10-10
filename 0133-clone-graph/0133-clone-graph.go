0670b3227372eb59731b0e53272636d935b7db0a
Vince <vincivenv@gmail.com>
2023-10-10 12:51:30 +0200

    Time: 3 ms (36.33%), Space: 2.9 MB (82.57%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.go
+++ 0133-clone-graph/0133-clone-graph.go
@@ -10,26 +10,31 @@ func cloneGraph(node *Node) *Node {
     if node == nil {
         return nil
     }
-    
-    visited := make(map[*Node]*Node)
-    return dfs(node, visited)
-}
 
-func dfs(node *Node, visited map[*Node]*Node) *Node {
-    // If the node has been visited, then return the clone from the visited map.
-    if clone, ok := visited[node]; ok {
-        return clone
+    visited := make(map[*Node]*Node)
+    // Create a clone for the starting node and add it to the visited map.
+    visited[node] = &Node{Val: node.Val, Neighbors: []*Node{}}
+    
+    // Initialize a queue with the original graph's starting node.
+    queue := []*Node{node}
+    
+    for len(queue) > 0 {
+        // Dequeue a node from the front of the queue.
+        current := queue[0]
+        queue = queue[1:]
+        
+        // Process the neighbors of the current node.
+        for _, neighbor := range current.Neighbors {
+            if _, ok := visited[neighbor]; !ok {
+                // If the neighbor hasn't been visited, create a clone, and enqueue the neighbor.
+                visited[neighbor] = &Node{Val: neighbor.Val, Neighbors: []*Node{}}
+                queue = append(queue, neighbor)
+            }
+            // Add the clone of the neighbor to the clone of the current node's neighbors list.
+            visited[current].Neighbors = append(visited[current].Neighbors, visited[neighbor])
+        }
     }
     
-    // Clone the current node.
-    clone := &Node{Val: node.Val, Neighbors: make([]*Node, len(node.Neighbors))}
-    // Mark the current node as visited.
-    visited[node] = clone
-    
-    // Process the neighbors.
-    for i, neighbor := range node.Neighbors {
-        clone.Neighbors[i] = dfs(neighbor, visited)
-    }
-    
-    return clone
+    // Return the clone of the starting node.
+    return visited[node]
 }
