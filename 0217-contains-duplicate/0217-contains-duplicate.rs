88564fea476099dcbce9c52c63141cd93701c360
Vince <vincivenv@gmail.com>
2024-01-11 12:35:50 +0100

    Time: 14 ms (68.78%), Space: 4.6 MB (9.68%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.rs
+++ 0217-contains-duplicate/0217-contains-duplicate.rs
@@ -1,6 +1,22 @@
+// O(n) -> time
 impl Solution {
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-        let non_dups: std::collections::HashSet<_> = nums.iter().collect();
-        non_dups.len() != nums.len()
+        let mut non_dups = std::collections::HashSet::new();
+
+        for num in nums.iter() {
+            if !non_dups.insert(num) {
+                return true;
+            }
+        }
+        return false;
     }
 }
+
+// O(n) -> time 
+// O(min(n,k)) where k is number of unique -> space
+// impl Solution {
+//     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+//         let non_dups: std::collections::HashSet<_> = nums.iter().collect();
+//         non_dups.len() != nums.len()
+//     }
+// }
