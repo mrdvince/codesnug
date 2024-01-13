f12c8b40e50017a4cc930a431f05b73982446e09
Vince <vincivenv@gmail.com>
2024-01-13 15:48:12 +0100

    Time: 3 ms (53.59%), Space: 2.3 MB (56.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.rs
+++ 0242-valid-anagram/0242-valid-anagram.rs
@@ -5,17 +5,32 @@ impl Solution {
             return false;
         }
 
-        let mut count_map = HashMap::new();
-        for ch in s.chars() {
-            *count_map.entry(ch).or_insert(0) += 1;
+        let mut count_map: HashMap<char, i32> = HashMap::new();
+        for (a, b) in s.chars().zip(t.chars()) {
+            *count_map.entry(a).or_insert(0) += 1;
+            *count_map.entry(b).or_insert(0) -= 1;
         }
-        for ch in t.chars(){
-            let count = count_map.entry(ch).or_insert(0);
-            if *count == 0 {
-                return false;
-            }
-            *count -= 1
-        }
-        true
+        count_map.values().all(|&val| val == 0)
     }
 }
+
+// impl Solution {
+//     pub fn is_anagram(s: String, t: String) -> bool {
+//         if s.len() != t.len() {
+//             return false;
+//         }
+
+//         let mut count_map = HashMap::new();
+//         for ch in s.chars() {
+//             *count_map.entry(ch).or_insert(0) += 1;
+//         }
+//         for ch in t.chars() {
+//             let count = count_map.entry(ch).or_insert(0);
+//             if *count == 0 {
+//                 return false;
+//             }
+//             *count -= 1
+//         }
+//         true
+//     }
+// }
