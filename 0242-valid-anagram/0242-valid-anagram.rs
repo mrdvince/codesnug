622a0e0c8291af4bdf2cca3ed24630c8b5b94c7c
Vince <vincivenv@gmail.com>
2024-01-13 15:38:31 +0100

    Time: 3 ms (53.59%), Space: 2.3 MB (56.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.rs
+++ 0242-valid-anagram/0242-valid-anagram.rs
@@ -1,13 +1,21 @@
+use std::collections::HashMap;
 impl Solution {
     pub fn is_anagram(s: String, t: String) -> bool {
         if s.len() != t.len() {
             return false;
         }
-        let mut s_vec:Vec<_> = s.chars().collect();
-        s_vec.sort();
-        let mut t_vec:Vec<_> = t.chars().collect();
-        t_vec.sort();
 
-        s_vec == t_vec
+        let mut count_map = HashMap::new();
+        for ch in s.chars() {
+            *count_map.entry(ch).or_insert(0) += 1;
+        }
+        for ch in t.chars(){
+            let count = count_map.entry(ch).or_insert(0);
+            if *count == 0 {
+                return false;
+            }
+            *count -= 1
+        }
+        true
     }
 }
