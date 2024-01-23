3c76213422cd06087f25bf5a501c542f44a7bd62
Vince <vincivenv@gmail.com>
2024-01-23 11:40:18 +0100

    Time: 3 ms (71.99%), Space: 3.5 MB (7.33%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.rs
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.rs
@@ -2,19 +2,46 @@ use std::collections::HashMap;
 impl Solution {
     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
         let mut counter: HashMap<i32, i32> = HashMap::new();
-        for num in nums {
+        for &num in nums.iter() {
             *counter.entry(num).or_insert(0) += 1;
         }
-        let mut counts: Vec<(i32, i32)> = counter.into_iter().collect();
-        // Sort by frequency in descending order
-        counts.sort_by(|a, b| b.1.cmp(&a.1));
-        println!("{:?}", counts);
+        let mut buckets: Vec<Vec<i32>> = vec![Vec::new(); nums.len() + 1];
+        for (&num, &freq) in counter.iter() {
+            buckets[freq as usize].push(num);
+        }
 
-        // Take top k elements
-        counts
-            .into_iter()
-            .map(|(num, _)| num)
-            .take(k as usize)
-            .collect()
+        let mut res = Vec::new();
+        for bucket in buckets.iter().rev() {
+            for &num in bucket {
+                res.push(num);
+                if res.len() == k as usize {
+                    return res;
+                }
+            }
+        }
+        res
     }
 }
+
+
+
+// use std::collections::HashMap;
+// impl Solution {
+//     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+//         let mut counter: HashMap<i32, i32> = HashMap::new();
+//         for num in nums {
+//             *counter.entry(num).or_insert(0) += 1;
+//         }
+//         let mut counts: Vec<(i32, i32)> = counter.into_iter().collect();
+//         // Sort by frequency in descending order
+//         counts.sort_by(|a, b| b.1.cmp(&a.1));
+//         println!("{:?}", counts);
+
+//         // Take top k elements
+//         counts
+//             .into_iter()
+//             .map(|(num, _)| num)
+//             .take(k as usize)
+//             .collect()
+//     }
+// }
