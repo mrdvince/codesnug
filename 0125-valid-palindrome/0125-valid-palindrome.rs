dd1ffb49b4a89615fcace24aab1d980b643753d3
Vince <vincivenv@gmail.com>
2024-07-01 08:27:04 +0200

    Time: 3 ms (19.33%), Space: 2.5 MB (23.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.rs
+++ 0125-valid-palindrome/0125-valid-palindrome.rs
@@ -1,24 +1,24 @@
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        let mut new_s = vec![];
-        for c in s.chars() {
-            if c.is_alphanumeric() {
-                new_s.push(c.to_lowercase().to_string())
-            }
-        }
+        let mut new_s: Vec<char> = s
+            .chars()
+            .filter(|c| c.is_alphanumeric())
+            .flat_map(|c| c.to_lowercase())
+            .collect();
+
         let mut left_ptr = 0;
-        let mut right_ptr = new_s.len() -1;
-        
+        let mut right_ptr = new_s.len() - 1;
+
         if new_s.is_empty() {
             return true;
         }
 
         while left_ptr < right_ptr {
-            if new_s[left_ptr] != new_s[right_ptr]{
+            if new_s[left_ptr] != new_s[right_ptr] {
                 return false;
             }
-            left_ptr +=1;
-            right_ptr -=1;
+            left_ptr += 1;
+            right_ptr -= 1;
         }
         true
     }
