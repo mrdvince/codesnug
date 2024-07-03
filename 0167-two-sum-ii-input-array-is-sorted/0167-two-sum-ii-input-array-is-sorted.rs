6267dfa30ad6feba7576b46fcb96424095ae4edf
Vince <vincivenv@gmail.com>
2024-07-03 08:42:08 +0200

    Time: 2 ms (51.38%), Space: 2.2 MB (96.13%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.rs
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.rs
@@ -4,17 +4,33 @@ impl Solution {
         let mut ri = numbers.len() - 1;
 
         while li < ri {
-            let curr_sum = numbers[li] + numbers[ri];
-            if curr_sum == target {
-                return vec![(li + 1) as i32, (ri + 1) as i32];
-            }
-            if curr_sum < target {
-                li += 1;
-            }
-            if curr_sum > target {
-                ri -= 1;
+            match (numbers[li] + numbers[ri]).cmp(&target) {
+                std::cmp::Ordering::Equal => return vec![(li + 1) as i32, (ri + 1) as i32],
+                std::cmp::Ordering::Less => li += 1,
+                std::cmp::Ordering::Greater => ri -= 1,
             }
         }
         unreachable!("never gets here")
     }
 }
+
+// impl Solution {
+//     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+//         let mut li = 0;
+//         let mut ri = numbers.len() - 1;
+
+//         while li < ri {
+//             let curr_sum = numbers[li] + numbers[ri];
+//             if curr_sum == target {
+//                 return vec![(li + 1) as i32, (ri + 1) as i32];
+//             }
+//             if curr_sum < target {
+//                 li += 1;
+//             }
+//             if curr_sum > target {
+//                 ri -= 1;
+//             }
+//         }
+//         unreachable!("never gets here")
+//     }
+// }
