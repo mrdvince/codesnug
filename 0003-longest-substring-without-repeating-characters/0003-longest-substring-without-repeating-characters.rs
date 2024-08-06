8fd3340c86c0dcf80335aa0797f4669bde943606
Vince <vincivenv@gmail.com>
2024-08-06 11:21:07 +0200

    Time: 0 ms (100%), Space: 2.3 MB (28.72%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
@@ -1,12 +1,15 @@
 use std::collections::HashSet;
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let mut ans = 0;
-        let mut left = 0;
+        if s.len() == 0 {
+            return 0;
+        }
+        let chars: Vec<char> = s.chars().collect();
+        let (mut ans, mut left) = (0, 0);
         let mut chars_set = HashSet::new();
-        for (right, c) in s.chars().enumerate() {
+        for (right, &c) in chars.iter().enumerate() {
             while chars_set.contains(&c) {
-                chars_set.remove(&s.chars().nth(left).unwrap());
+                chars_set.remove(&chars[left]);
                 left += 1;
             }
             chars_set.insert(c);
