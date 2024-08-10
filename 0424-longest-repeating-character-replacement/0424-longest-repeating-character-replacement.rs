4b688741d95a357993aa33076814974e41825d68
Vince <vincivenv@gmail.com>
2024-08-10 12:51:38 +0200

    Time: 2 ms (75.32%), Space: 2.1 MB (59.74%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.rs
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.rs
@@ -3,12 +3,15 @@ impl Solution {
     pub fn character_replacement(s: String, k: i32) -> i32 {
         let s = s.chars().collect::<Vec<char>>();
         let mut count = HashMap::new();
-        let (mut left, mut res) = (0, 0);
+        let (mut left, mut res, mut max_freq) = (0, 0, 0);
 
         for right in 0..s.len() {
-            count.entry(s[right]).and_modify(|c| *c += 1).or_insert(1);
-            if (right - left + 1) as i32 - *count.values().max().unwrap() > k {
-                count.entry(s[left]).and_modify(|c| *c -= 1);
+            *count.entry(s[right]).or_insert(0) += 1;
+
+            max_freq = max_freq.max(*count.get(&s[right]).unwrap());
+
+            if (right - left + 1) as i32 - max_freq > k {
+                *count.get_mut(&s[left]).unwrap() -= 1;
                 left += 1
             }
             res = res.max(right - left + 1);
